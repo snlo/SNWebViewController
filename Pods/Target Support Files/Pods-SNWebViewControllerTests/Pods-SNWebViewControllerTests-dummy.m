946fba75f554a8f778be07ea388c3905340f22c3
snlo <snloveydus@sina.com>
2019-04-03 18:28:08 +0800

    移除'Pods'文件夹

--- Pods/Target Support Files/Pods-SNWebViewControllerTests/Pods-SNWebViewControllerTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SNWebViewControllerTests : NSObject
-@end
-@implementation PodsDummy_Pods_SNWebViewControllerTests
-@end
--- Pods/Target Support Files/Pods-SNWebViewControllerUITests/Pods-SNWebViewControllerUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SNWebViewControllerUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SNWebViewControllerUITests
-@end
